d6145b470f7fa42dd5532553e795afe4ec84dbc5
Tommy <reesekin@yahoo.com>
2022-10-26 14:13:58 -0700

    services page

--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -1,4 +1,11 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
+import { Login } from 'src/login/login.entity';
 
-@Module({})
+@Module({
+  imports: [forwardRef(() => AuthModule), TypeOrmModule.forFeature([Login])],
+  providers: [],
+  exports: [],
+})
 export class ServicesModule {}
